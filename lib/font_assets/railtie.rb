306a87f2035ecbac30775b5b4ac3ca9f49a7f51e
phereford <patrick.hereford@americastestkitchen.com>
2013-04-30 13:56:18 -0400

    Threadsafe complete

--- lib/font_assets/railtie.rb
+++ lib/font_assets/railtie.rb
@@ -8,7 +8,7 @@ module FontAssets
       config.font_assets.origin ||= "*"
       config.font_assets.options ||= { allow_ssl: true }
 
-      app.middleware.insert_before 'Rack::Lock', FontAssets::Middleware, config.font_assets.origin, config.font_assets.options
+      app.middleware.insert_before 'Rack::Runtime', FontAssets::Middleware, config.font_assets.origin, config.font_assets.options
     end
   end
 end
